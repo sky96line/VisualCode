9cbef95371bcd8ef2d95cdfd9123d6747d1e53a8
Akash Kansara <skyline_96@live.com>
2017-10-12 21:45:07 +0530

    Rod Cutting Problem

---
+++ RodCutting/rod.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void display(int table[], int target)
+{
+  int i = 0;
+  for (i = 0; i < target;i++)
+    printf(" %d", table[i]);
+}
+int MinRod(int target, int len[], int profit[])
+{
+  int i = 0, j = 0;
+  int size = sizeof(len);
+  int table[target + 1];
+  for (i = 0; i <= target; i++)
+  {
+    table[i] = i;
+  }
+  for (i = 1; i <= target;i++)
+  {
+    display(table, target);
+    for (j = 0; j < size;j++)
+    {
+      if(len[j]<=i)
+      {
+        int sub = table[i - len[j]];
+        if(sub+profit[j]>table[i])
+          table[i] = sub + profit[j];
+      }
+    }
+    printf("\n");
+  }
+  return table[target];
+}
+
+int main(void)
+{
+  int len[] = {1, 2, 3};
+  int profit[] = {1, 5, 8};
+  int target = 5;
+  int result = MinRod(target, len, profit);
+  printf("%d", result);
+  return 0;
+}
